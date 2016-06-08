09370ae4909a856c0bc54eb05951f76a4b1fdcb1
Kim Chanwoo <edcwkim@gmail.com>
2016-06-08 15:58:28 +0900

    회원가입 기능 추가

--- chanwoo/accounts/urls.py
+++ chanwoo/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as django_views
+from . import views
 
 urlpatterns = [
     url(r'^login/$', django_views.login, name='login'),
     url(r'^logout/$', django_views.logout, name='logout'),
+    url(r'^register/$', views.Register.as_view(), name='register'),
 ]
--- chanwoo/accounts/views.py
+++ chanwoo/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
 
-# Create your views here.
+
+class Register(generic.CreateView):
+
+    model = get_user_model()
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+
+    def get_success_url(self):
+        return settings.LOGIN_URL
